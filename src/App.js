cd4b27c990afa8cf619c1f59cd81086dd88e0d81
IlP4rAd0xlI <elia.portela5@gmail.com>
2025-06-11 15:14:55 +0200

    Init files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p className="bg-black rounded-lg p-3">Hello World</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
