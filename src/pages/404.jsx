4d42122aebee6120f81ac78a72ba9952d861ec84
Anduan <183530204+AnduanBerisha@users.noreply.github.com>
2025-06-16 10:44:16 +0200

    Add: Page 404 + btn redirection Home

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,9 +1,18 @@
-import { Link } from 'react-router-dom';
+import { Link, Route, Routes, useNavigate } from 'react-router-dom';
+import react from 'react';
 
 function Page404() {
+  const navigate = UseNavigate();
+
+  const goHome = () => {
+    navigate('/Home.jsx')
+  }
+  
   return (
     <>
-        <h1>404</h1>
+        <h1>404 - Page non trouvée</h1>
+        <p>texte</p>
+        <button onClick={goHome}>Retourner à l'accueil</button>
     </>
   );
 }
