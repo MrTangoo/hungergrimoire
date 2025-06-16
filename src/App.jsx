69da8f42e59f02ff9e1e12dcd95bf87f0353ac6d
Aleksa Mihajlovic <aleksamihajlovic983@gmail.com>
2025-06-16 09:14:02 +0200

    Add : Routage entre pages

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Recettes from './pages/Recettes'
+import About from './pages/About'
+import Page404 from './pages/404'
+
+function App() {
+  return (
+    <>
+      <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/Home' element={<Home />}/>
+          <Route path='/Recettes' element={<Recettes />}/>
+          <Route path='/About' element={<About />}/>
+          <Route path='/404' element={<Page404 />}/>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
