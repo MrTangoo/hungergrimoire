976281d3799eddd24005b9ed676793a2febcf671
Anduan <183530204+AnduanBerisha@users.noreply.github.com>
2025-06-16 13:33:00 +0200

    Move : Dossier media/wizzard -> src/wizzard

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
-import react from 'react';
+import sorcier404 from '../wizard/sad-404.png';
+import arbre404 from '../arbre-404.png';
 
 function Page404() {
   const navigate = useNavigate();
@@ -9,11 +10,19 @@ function Page404() {
   }
   
   return (
-    <>
+    <div
+      className="h-screen bg-center bg-no-repeat" 
+      style={{ 
+        backgroundImage: `url(${arbre404})`, // utiliser les `` (shift + ^ + espace) quand t'importe qlq chose en style
+        backgroundSize: '2000px 400px', // d'abord largeur ensuite hauteur
+        backgroundPosition: 'center bottom 20px',
+      }}
+    >
         <h1>404 - Page non trouvée</h1>
         <p>texte</p>
         <button onClick={goHome}>Retourner à l'accueil</button>
-    </>
+        <img src={sorcier404} alt="image sorcier 404" />
+    </div>
   );
 }
 
