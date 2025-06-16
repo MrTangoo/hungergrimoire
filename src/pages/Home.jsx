0dc895b622347b7d88aed55c0dd0a91f7c27e3db
Aleksa Mihajlovic <aleksamihajlovic983@gmail.com>
2025-06-16 10:09:57 +0200

    Add: bar de recherche

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom';
+import Header from '../components/Header'
 
 function Home() {
   return (
     <>
+        <Header />
         <h1>salut</h1>
         <Link to="./About">Lien about</Link>
     </>
