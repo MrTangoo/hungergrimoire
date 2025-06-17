65743bd470a314dae2a3bb905115f2acce5f877e
IlP4rAd0xlI <elia.portela5@gmail.com>
2025-06-17 08:36:28 +0200

    Create : Card

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import test_img from '../img/test_img.jpg';
+
+function Card({cardColor, cardImg, reciepName, ingredients, nbPeople, reciepDuration, reciepUrl}) {
+  return (
+    <>
+        <div class="w-1/4 bg-dark-green border-8 border-light-green rounded-xl">
+            <img class ="w-2/3 rounded-lg" src={cardImg} />
+            <p></p>
+        </div>
+    </>
+  );
+}
+
+export default Card;
