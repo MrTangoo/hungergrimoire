8dfc7a53eddb5213e7b934c6d4ba7fd999e982c3
Anduan <183530204+AnduanBerisha@users.noreply.github.com>
2025-06-17 14:43:12 +0200

    Add : Chrono comme page (temporaire)

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Recettes from './pages/Recettes';
 import About from './pages/About';
 import Page404 from './pages/404';
+import Chrono from './components/Chrono' // import du chrono, c'est temporaire, juste histoire que je puisse mieux voir les modifications
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path='/Home' element={<Home />}/>
           <Route path='/Recettes' element={<Recettes />}/>
           <Route path='/About' element={<About />}/>
+          <Route path='/Chrono' element={<Chrono />} />
           <Route path='*' element={<Page404 />} />
       </Routes>
     </>
