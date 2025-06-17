df00c85777eb8166ddf4f2f2b3d68db0f213a11d
IlP4rAd0xlI <elia.portela5@gmail.com>
2025-06-17 08:27:16 +0200

    Add : palette de couleurs

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,19 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'orange': '#F1BA88',
+      'yellow': '#E9F5BE',
+      'light-green': '#81E7AF',
+      'dark-green': '#03A791',
+      'lighter-grey': '#E4E4E4',
+      'light-grey': '#C7C7C7',
+      'medium-grey': '#9F9F9F',
+      'dark-grey': '#6E6E6E',
+      'dark2-grey': '#717171',
+      'transparent': 'transparent',
+      'white': 'white',
+    }
   },
   plugins: [],
 }
