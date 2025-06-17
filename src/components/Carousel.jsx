497b8b729ee1c1d61f0d28dc9c16f19dccf39832
IlP4rAd0xlI <elia.portela5@gmail.com>
2025-06-17 10:54:52 +0200

    Add : display card in carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,6 +2,13 @@ import { Link } from 'react-router-dom';
 import Card from './Card'
 import React from 'react';
 
+function Carousel() {
   return (
+    <>
+      <h1>CAROUSEL</h1>
+      <Card cardImg="../img/wizard_darkgreen.png" reciepName="Lasagnes" />
+    </>
   );
+}
 
+export default Carousel;
