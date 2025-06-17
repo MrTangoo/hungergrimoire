250b8ee17b2a63a33fa59d1a48c4a00d98db4366
Anduan <183530204+AnduanBerisha@users.noreply.github.com>
2025-06-17 15:59:57 +0200

    Update Chrono.jsx

--- src/components/Chrono.jsx
+++ src/components/Chrono.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import { useState, useEffect } from 'react';
+import { useTimer } from 'react-timer-hook';
+import PlaySvg from '../img/svg/play.svg';
+import StopSvg from '../img/svg/square.svg';
 
-function Chrono() {
+function Chrono({ MinutesCustom = 5 }) { // paramètres pour changer les minutes après, je met 5min pour test
+    
     return(
-        <>
-            
-        </>
+        <div className="bg-light-green h-500px flex flex-col items-center justify-center">
+            <div>
+                <p>test</p>
+            </div>
+        </div>
     );
 }
 
-export default Chrono();
+export default Chrono;
