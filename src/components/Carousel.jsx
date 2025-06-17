f849fd658454f905c21e201e4e5147abbebaa42d
IlP4rAd0xlI <elia.portela5@gmail.com>
2025-06-17 11:14:43 +0200

    Add : card img import into carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,14 @@
 import { Link } from 'react-router-dom';
 import Card from './Card'
+import test from '../img/wizard_darkgreen.png'
 import React from 'react';
 
 function Carousel() {
   return (
-    <>
+    <div className='w-full flex justify-center items-center p-[2%]'>
       <h1>CAROUSEL</h1>
-      <Card cardImg="../img/wizard_darkgreen.png" reciepName="Lasagnes" />
-    </>
+      <Card cardImg={test} reciepName="Lasagnes" nbPeople="4" reciepUrl="./404" />
+    </div>
   );
 }
 
