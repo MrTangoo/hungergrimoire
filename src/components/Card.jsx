6f59b6b3bcf072296e86215068291e88719b4c60
IlP4rAd0xlI <elia.portela5@gmail.com>
2025-06-17 14:42:49 +0200

    Update : Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,9 +4,9 @@ function Card({cardColor, cardImg, reciepName, ingredients, nbPeople, reciepDura
   return (
     <>
         <div className='flex items-center justify-center w-80 h-96 bg-light-green rounded-3xl'>
-          <div className='h-full flex flex-col items-center justify-around bg-dark-green scale-90 rounded-3xl'>
-            <div className='scale-90 flex flex-col items-center'>
-              <img className='' src={cardImg} />
+          <div className='h-full bg-dark-green scale-90 rounded-3xl'>
+            <div className='h-full flex flex-col items-center justify-between scale-90'>
+              <img className='rounded-3xl' src={cardImg} />
               <h1 className='flex items-center justify-center bg-light-green'>{reciepName}</h1>
               <p>{nbPeople} personnes</p>
               <Link className='flex items-center justify-center bg-light-grey' to={reciepUrl}>Ouvrir</Link>
