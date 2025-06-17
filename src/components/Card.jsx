2fbb2106e82a84342dbe9719d9c50cf8b5a2a580
IlP4rAd0xlI <elia.portela5@gmail.com>
2025-06-17 13:11:46 +0200

    Update : change card sizes units

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom';
 function Card({cardColor, cardImg, reciepName, ingredients, nbPeople, reciepDuration, reciepUrl}) {
   return (
     <>
-        <div className='flex items-center justify-center w-1/6 h-96 bg-light-green rounded-[15%]'>
-          <div className='flex flex-col items-center justify-around w-[90%] h-[92%] bg-dark-green rounded-[15%]'>
-            <img className='w-2/3 rounded-lg' src={cardImg} />
-            <h1 className='flex items-center justify-center bg-light-green rounded-lg w-2/3 h-9 px-5'>{reciepName}</h1>
-            <p>{nbPeople} personnes</p>
-            <Link className='flex items-center justify-center bg-light-grey rounded-lg h-9 px-5' to={reciepUrl}>Ouvrir</Link>
+        <div className='flex items-center justify-center w-80 h-96 bg-light-green rounded-3xl'>
+          <div className='h-full flex flex-col items-center justify-around bg-dark-green scale-90 rounded-3xl'>
+            <div className='scale-90 flex flex-col items-center'>
+              <img className='' src={cardImg} />
+              <h1 className='flex items-center justify-center bg-light-green'>{reciepName}</h1>
+              <p>{nbPeople} personnes</p>
+              <Link className='flex items-center justify-center bg-light-grey' to={reciepUrl}>Ouvrir</Link>
+            </div>
           </div>
         </div>
     </>
