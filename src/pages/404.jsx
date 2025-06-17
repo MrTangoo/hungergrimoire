188454bfdb6b98a58456de01254661d5010e0fc8
Anduan <183530204+AnduanBerisha@users.noreply.github.com>
2025-06-17 08:28:51 +0200

    Update 404.jsx
    J'enlève les anciens bouts de code qui sont pas utile

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,6 +1,4 @@
 import { useNavigate } from 'react-router-dom';
-import sorcier404 from '../wizard/sad-404.png';
-import arbre404 from '../arbre-404.png';
 
 function Page404() {
   const navigate = useNavigate();
@@ -8,20 +6,7 @@ function Page404() {
   const goHome = () => {
     navigate('/')
   }
-  
   return (
-    <div
-      className="h-screen bg-center bg-no-repeat" 
-      style={{ 
-        backgroundImage: `url(${arbre404})`, // utiliser les `` (shift + ^ + espace) quand t'importe qlq chose en style
-        backgroundSize: '2000px 400px', // d'abord largeur ensuite hauteur
-        backgroundPosition: 'center bottom 20px',
-      }}
-    >
-        <h1>404 - Page non trouvée</h1>
-        <p>texte</p>
-        <button onClick={goHome}>Retourner à l'accueil</button>
-        <img src={sorcier404} alt="image sorcier 404" />
     </div>
   );
 }
