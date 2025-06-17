92333d58a2b992ca6ac7e85a9f40438125f9b624
Anduan <183530204+AnduanBerisha@users.noreply.github.com>
2025-06-17 08:29:51 +0200

    Update 404.jsx
    On ajoute d'autres fonctionnalités plus jolie suite au changement de la maquette/ du design

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+import sorcier from '../image404-sorcier-orage.png';
 
 function Page404() {
   const navigate = useNavigate();
@@ -6,7 +7,28 @@ function Page404() {
   const goHome = () => {
     navigate('/')
   }
+
   return (
+    // p c'est padding
+    <div className="bg-cream flex justify-between items-center p-8">
+      <div>
+        <h1 className="text-center font-bold text-7xl">Erreur 404</h1>
+        <p className="text-3xl">Oulala! La page que vous cherchez n'existe pas!</p>
+        <br />
+        <p className="text-3xl">
+          À la place vous avez trouvé le sorcier triste! 
+          Laissez-moi vous ramener à un endroit plus sûr.
+        </p>
+        <button onClick={goHome}
+          className=""
+        >
+          Retourner à l'accueil
+        </button>
+      </div>
+
+      <img src={sorcier} alt="image sorcier sous orage" 
+        className=""
+      />
     </div>
   );
 }
