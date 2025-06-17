6baafbfa1d526987cfc42f15b8bf44ae0ac48f7d
IlP4rAd0xlI <elia.portela5@gmail.com>
2025-06-17 11:02:48 +0200

    Update : class to className

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,9 +1,9 @@
 function Title() {
   return (
     <>
-        <div class="flex flex-col">
-            <h1 class="text-center">Hunger Grimoire</h1>
-            <h2 class="text-center">Invoque ton festin avec ce que cache ton garde-manger</h2>
+        <div className="flex flex-col">
+            <h1 className="text-center">Hunger Grimoire</h1>
+            <h2 className="text-center">Invoque ton festin avec ce que cache ton garde-manger</h2>
         </div>
     </>
   );
