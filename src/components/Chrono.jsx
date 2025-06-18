002f6ca631296893749b9582b54d0d9371b1c399
Anduan <183530204+AnduanBerisha@users.noreply.github.com>
2025-06-18 09:46:50 +0200

    Update Chrono.jsx

--- src/components/Chrono.jsx
+++ src/components/Chrono.jsx
@@ -2,13 +2,36 @@ import { useState, useEffect } from 'react';
 import { useTimer } from 'react-timer-hook';
 import PlaySvg from '../img/svg/play.svg';
 import StopSvg from '../img/svg/square.svg';
+import RestartSvg from '../img/svg/restart.svg';
 
 function Chrono({ MinutesCustom = 5 }) { // paramètres pour changer les minutes après, je met 5min pour test
     
+    const time = new Date(); // on crée la variable du temps
+    time.setSeconds(time.getSeconds() + MinutesCustom * 60) // on convertis les secondes en minutes
+
+    const {
+        seconds,
+        minutes,
+        isRunning,
+        start,
+        pause,
+        resume,
+        restart,
+    } = useTimer({ expiryTimestamp: time });
+    // expiryTimestamp veut dire que le temps est terminé par la spécification de time
+
     return(
         <div className="bg-light-green h-500px flex flex-col items-center justify-center">
+            <div className="text-4xl">
+                {minutes}:{seconds < 10 ? `0${seconds}` : seconds} {/* opérateur ternaire, si sec < 10 alors ajt 0 devant, par exemple 9 sec -> 09 sec */}
+            </div>
             <div>
-                <p>test</p>
+                <button onClick={isRunning ? pause : start} className="mr-5">
+                    <img src={isRunning ? StopSvg : PlaySvg} alt={isRunning ? "Stop" : "Play"} />
+                </button>
+                <button onClick={() => restart(time)}>
+                    <img src={RestartSvg} alt="Restart" />
+                </button>
             </div>
         </div>
     );
