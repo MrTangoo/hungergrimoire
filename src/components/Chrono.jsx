98da99df16e2009648553f0088d1c7659ccb3e44
Anduan <183530204+AnduanBerisha@users.noreply.github.com>
2025-06-18 14:19:00 +0200

    Update Design Chrono.jsx

--- src/components/Chrono.jsx
+++ src/components/Chrono.jsx
@@ -20,17 +20,19 @@ function Chrono({ MinutesCustom = 5 }) { // paramètres pour changer les minutes
     } = useTimer({ expiryTimestamp: time});
 
     return(
-        <div className="bg-light-green h-500px flex flex-col items-center justify-center">
-            <div className="text-4xl">
-                {minutes}:{seconds < 10 ? `0${seconds}` : seconds} {/* opérateur ternaire, si sec < 10 alors ajt 0 devant, par exemple 9 sec -> 09 sec */}
-            </div>
-            <div>
-                <button onClick={isRunning ? pause : resume} className="mr-5">
-                    <img src={isRunning ? StopSvg : PlaySvg} alt={isRunning ? "Stop" : "Play"} />
-                </button>
-                <button onClick={() => restart(time)}>
-                    <img src={RestartSvg} alt="Restart" />
-                </button>
+        <div className="flex justify-center items-center h-screen w-screen">
+            <div className="bg-dark-grey rounded-full p-4 w-80 flex items-center justify-center">
+                <div className="text-2xl text-white items-center mr-20">
+                    {minutes}:{seconds < 10 ? `0${seconds}` : seconds} {/* opérateur ternaire, si sec < 10 alors ajt 0 devant, par exemple 9 sec -> 09 sec */}
+                </div>
+                <div className="flex">
+                    <button onClick={isRunning ? pause : resume} className="mr-5">
+                        <img src={isRunning ? StopSvg : PlaySvg} alt={isRunning ? "Stop" : "Play"} />
+                    </button>
+                    <button onClick={() => restart(time)}>
+                        <img src={RestartSvg} alt="Restart" />
+                    </button>
+                </div>
             </div>
         </div>
     );
