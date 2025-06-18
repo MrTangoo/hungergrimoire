5943f57ecad270f65663b5bebd183cb85de19ced
Anduan <183530204+AnduanBerisha@users.noreply.github.com>
2025-06-18 14:31:35 +0200

    Remove /chrono path

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Home from './pages/Home';
 import Recettes from './pages/Recettes';
 import About from './pages/About';
 import Page404 from './pages/404';
-import Chrono from './components/Chrono'; // import du chronometre, c'est temporaire, juste histoire que je puisse mieux voir les modifications
 
 function App() {
   return (
@@ -15,7 +14,6 @@ function App() {
           <Route path='/Home' element={<Home />}/>
           <Route path='/Recettes' element={<Recettes />}/>
           <Route path='/About' element={<About />}/>
-          <Route path='/Chrono' element={<Chrono />} />
           <Route path='*' element={<Page404 />} />
       </Routes>
     </>
