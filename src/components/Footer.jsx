4898b1685ef4dc2e13355f44a8f13c16113ccfaa
IlP4rAd0xlI <elia.portela5@gmail.com>
2025-06-23 15:04:59 +0200

    Update : Footer fonts

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
                 <div className="w-1/4 flex flex-col items-start">
                     <h1 className="text-xl  mb-5" style={{ fontFamily: 'Megrim'}}>The Hunger Grimoire</h1>
                     <div className='flex flex-col items-start'>
-                        <h2 className='text-md' style={{}}>Simple, rapide, efficace.</h2>
+                        <h2 className='text-md'>Simple, rapide, efficace.</h2>
                         <h3 style={{ fontFamily: 'Inria_Sans-Light'}} className='text-xs'>Bon appétit!</h3>
                     </div>
                 </div>
@@ -17,12 +17,12 @@ function Footer() {
 
                 <div className="w-1/4 flex justify-end">
                     <div className='flex flex-col justify-center'>
-                        <h1 className="font-bold text-base mb-4 text-center">Notre équipe</h1>
+                        <h1 style={{ fontFamily: 'Inria_Sans-Bold'}} className="text-base mb-4 text-center">Notre équipe</h1>
                         <div className="flex text-xl gap-2 transition-all duration-1000 ease-in-out hover:scale-110">
-                            <a className='transition-all duration-900 ease-in-out hover:scale-110 bg-dark-green h-10 w-10 rounded-full flex justify-center items-center' href="https://github.com/MathieuBurnat">M</a>
-                            <a className='transition-all duration-900 ease-in-out hover:scale-110 bg-yellow h-10 w-10 rounded-full flex justify-center items-center' href="https://github.com/AnduanBerisha">An</a>
-                            <a className='transition-all duration-900 ease-in-out hover:scale-110 bg-light-green h-10 w-10 rounded-full flex justify-center items-center' href="https://github.com/lexas220">Al</a>
-                            <a className='transition-all duration-900 ease-in-out hover:scale-110 bg-orange h-10 w-10 rounded-full flex justify-center items-center' href="https://github.com/IlP4rAd0xlI">E</a>
+                            <a className='transition-all duration-900 ease-in-out hover:scale-110 bg-dark-green h-10 w-10 rounded-full flex justify-center items-center' href="https://github.com/MathieuBurnat">MB</a>
+                            <a className='transition-all duration-900 ease-in-out hover:scale-110 bg-yellow h-10 w-10 rounded-full flex justify-center items-center' href="https://github.com/AnduanBerisha">AB</a>
+                            <a className='transition-all duration-900 ease-in-out hover:scale-110 bg-light-green h-10 w-10 rounded-full flex justify-center items-center' href="https://github.com/lexas220">AM</a>
+                            <a className='transition-all duration-900 ease-in-out hover:scale-110 bg-orange h-10 w-10 rounded-full flex justify-center items-center' href="https://github.com/IlP4rAd0xlI">EP</a>
                         </div>
                     </div>
                 </div>
