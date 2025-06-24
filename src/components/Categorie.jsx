694c94e7ec2aad0aba8102e7fd6e97d09b6e7973
IlP4rAd0xlI <ilp4rad0xli@gmail.com>
2025-06-24 10:21:34 +0200

    Create : Categorie component

--- src/components/Categorie.jsx
+++ src/components/Categorie.jsx
@@ -6,6 +6,7 @@ function Categorie ( {categorieName} ) {
         <h1 className='bg-dark-grey text-lighter-grey text-lg rounded-full py-2 w-1/5 text-center mt-20'>{categorieName}</h1>
         <Carousel 
           carouselType={"smallCards"}
+          displayIngredients={"collapse"}
         />
     </>
     );
