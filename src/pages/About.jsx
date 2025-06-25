ed8db63b01dc9149f1522bf83fb96b99bf370fb7
IlP4rAd0xlI <ilp4rad0xli@gmail.com>
2025-06-25 11:32:36 +0200

    Add : wizard to About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom';
 import Recette from '../components/Recette'
+import darkgreen_wizard from '../img/wizard/wizard_darkgreen.png';
 
 function About() {
   return (
     <>
-        <h1 className='w-full h-96 flex flex-col justify-center items-center text-7xl'>About</h1>
+      <img className='w-32 absolute top-4 right-3 z-20' src={darkgreen_wizard}></img>
+      <h1 className='w-full h-96 flex flex-col justify-center items-center text-7xl'>About</h1>
     </>
   );
 }
