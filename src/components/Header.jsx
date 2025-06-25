e4f7d880d2667ecc9ae2cc9d0a2da20a1e03ef7e
IlP4rAd0xlI <ilp4rad0xli@gmail.com>
2025-06-25 10:34:00 +0200

    Resolve : merge header problem
    There was 2 search bars

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -54,35 +54,20 @@ function Header() {
   return (
       <div className="h-16 absolute top-0 z-10 w-full flex items-center justify-start bg-light-grey px-5">
         <DropDownNav />
-        <div className="absolute xl:right-52 right-36 bg-dark-grey rounded-xl">
-          <input
-            className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm rounded-xl pl-3 pr-28 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
-            placeholder="Recherche de recettes"
-          />
-          <button
-            className="absolute top-1 right-1 flex items-center rounded bg-light-grey py-1 px-2.5 border border-transparent text-center text-sm text-white transition-all shadow-sm hover:shadow focus:bg-dark2-grey focus:shadow-none active:bg-dark2-grey hover:bg-dark2-grey active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-            type="button"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4 mr-2">
-              <path fillRule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clipRule="evenodd" />
-            </svg>
-            Rechercher
-          </button>
-        </div>
         {/* Tes liens About / Recettes */}
 
-      <div className="relative basis-1/2 max-w-lg">
+      <div className="absolute right-40 max-w-lg bg-dark-grey rounded-lg">
         <form onSubmit={handleSearch}>
           <input
             type="text"
-            placeholder="Rechercher une recette..."
+            placeholder="Recherche une recette"
             value={searchQuery}
             onChange={onChangeSearch}
-            className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md pl-3 pr-28 py-2"
+            className="w-full bg-transparent placeholder:text-light-grey text-white text-sm rounded-lg pl-3 pr-24 py-2"
           />
           <button
             type="submit"
-            className="absolute top-1 right-1 flex items-center rounded bg-dark-grey py-1 px-2.5 text-white text-sm"
+            className="absolute top-1 right-1 flex items-center rounded bg-light-grey py-1 px-2.5 text-black text-sm"
           >
             Rechercher
           </button>
